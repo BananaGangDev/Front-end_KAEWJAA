fc7b0dde8aa053f22168ef699da989208d6a2bb7
NICHAPA <nichapa.por@dome.tu.ac.th>
2024-03-18 16:29:41 +0700

    Edit innit

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,30 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Document from "./pages/Document";
+import Tagset from "./pages/Tagset";
+import Logout from "./pages/Logout";
+import Concordance from "./pages/Concordance";
+import './style/main.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Document />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/concordance" element={<Concordance />} />
+          <Route path="/tagset" element={<Tagset />} />
+          <Route path="/logout" element={<Logout />} />
+
+          <Route path="*" element={<> not found</>} />
+        </Routes>
+      </BrowserRouter>
   </React.StrictMode>,
+  
+
+
 )
+  
