c92526018464b4fd25d1521f549c6d7c40a853a6
Kanpitcha Hong-ek <99486954+lollipopeyes@users.noreply.github.com>
2024-03-18 16:38:19 +0700

    2nd push (login, reset password, import file)

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './index.css'
+// import './styles/main.css';
+
+import LoginForm from '/src/pages/LoginForm.jsx';
+import ResetPassword from '/src/pages/ResetPassword.jsx';
+import ImportDefault from '/src/pages/ImportDefault.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<LoginForm />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path="/import" element={<ImportDefault />} />
+
+        <Route path="*" element={<> not found</>} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
