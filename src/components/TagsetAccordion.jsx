c8796df3c5b70e0bc2687535834824ba55ff97a3
Aikodesu11 <pin6229@gmail.com>
2024-04-26 13:56:42 +0700

    Edit tagset css and set tagset parent/child

--- src/components/TagsetAccordion.jsx
+++ src/components/TagsetAccordion.jsx
@@ -1,116 +1,21 @@
-// import React, { useState } from 'react';
-// import Modal from 'react-bootstrap/Modal';
-// import Button from 'react-bootstrap/Button';
-// import Form from 'react-bootstrap/Form';
-// import AccordionSummary from '@mui/material/AccordionSummary';
-// import BookmarkIcon from '@mui/icons-material/Bookmark';
-// import EditIcon from '@mui/icons-material/Edit';
-// import Card from '@mui/material/Card';
-
-// function TagsetAccordion({ id, name, description, onUpdate }) {
-//   const [isBookmarked, setIsBookmarked] = useState(false);
-//   const [showEditModal, setShowEditModal] = useState(false);
-//   const [editedName, setEditedName] = useState(name);
-//   const [editedDescription, setEditedDescription] = useState(description);
-
-//   const handleBookmarkClick = () => {
-//     setIsBookmarked(!isBookmarked);
-//   };
-
-//   const handleEditClick = () => {
-//     setEditedName(name);
-//     setEditedDescription(description);
-//     setShowEditModal(true);
-//   };
-
-//   const handleSaveEdit = () => {
-//     // Perform update operation here with editedName and editedDescription
-//     onUpdate(id, editedName, editedDescription);
-//     setShowEditModal(false);
-//   };
-
-//   const handleCloseModal = () => {
-//     setShowEditModal(false);
-//   };
-
-//   return (
-//     <div>
-//       <Card className='tagset-accordion' style={{ backgroundColor: "#E7E5FF", marginBottom: "15px"}}>
-//         <AccordionSummary>
-//           <div className='tagset-content'>{name}</div>
-//           <div className='tagset-content'>{description}</div>
-//           <div className='tagset-button'>
-//             <BookmarkIcon
-//               onClick={handleBookmarkClick}
-//               style={{ color: isBookmarked ? '#FC5B5C' : 'inherit' }}
-//             />
-//             <EditIcon onClick={handleEditClick} />
-//           </div>
-//         </AccordionSummary>
-//       </Card>
-
-//       {/* Edit Modal */}
-//       <Modal className='create-modal' show={showEditModal} onHide={handleCloseModal}>
-//         <Modal.Header>
-//           <Modal.Title>Edit Tagset</Modal.Title>
-//         </Modal.Header>
-//         <Modal.Body>
-//           <Form>
-//             <Form.Group className="mb-3" controlId="formName">
-//               <Form.Label>Name</Form.Label>
-//               <Form.Control
-//                 type="text"
-//                 value={editedName}
-//                 onChange={(e) => setEditedName(e.target.value)}
-//                 placeholder="Edit tagset name"
-//               />
-//             </Form.Group>
-//             <Form.Group className="mb-3" controlId="formDescription">
-//               <Form.Label>Description</Form.Label>
-//               <Form.Control
-//                 as="textarea"
-//                 rows={3}
-//                 value={editedDescription}
-//                 onChange={(e) => setEditedDescription(e.target.value)}
-//                 placeholder="Edit tagset description"
-//               />
-//             </Form.Group>
-//           </Form>
-//         </Modal.Body>
-//         <Modal.Footer>
-//           <Button variant="secondary" onClick={handleCloseModal}>
-//             Close
-//           </Button>
-//           <Button variant="primary" onClick={handleSaveEdit}>
-//             Save Changes
-//           </Button>
-//         </Modal.Footer>
-//       </Modal>
-//     </div>
-//   );
-// }
-
-// export default TagsetAccordion;
-
 import React, { useState } from 'react';
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import AccordionSummary from '@mui/material/AccordionSummary';
-import BookmarkIcon from '@mui/icons-material/Bookmark';
-import EditIcon from '@mui/icons-material/Edit';
-import DeleteIcon from '@mui/icons-material/Delete';
-import Card from '@mui/material/Card';
-
+// import BookmarkIcon from '@mui/icons-material/Bookmark';
+import BookmarkBorderOutlinedIcon from '@mui/icons-material/BookmarkBorderOutlined';
+import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
+import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
+import CreateNewFolderOutlinedIcon from '@mui/icons-material/CreateNewFolderOutlined';
 import Accordion from '@mui/material/Accordion';
 import AccordionActions from '@mui/material/AccordionActions';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import CreateNewFolderIcon from '@mui/icons-material/CreateNewFolder';
-import NoteAddIcon from '@mui/icons-material/NoteAdd';
 
 
 
-function TagsetAccordion({ id, name, description, onUpdate, onDelete }) {
+function TagsetAccordion({ id, name, description, onUpdate, onDelete, onCreateNestedTagset, nestedTagsets  }) {
   const [isBookmarked, setIsBookmarked] = useState(false);
   const [showEditModal, setShowEditModal] = useState(false);
   const [showDeleteModal, setShowDeleteModal] = useState(false);
@@ -138,11 +43,11 @@ function TagsetAccordion({ id, name, description, onUpdate, onDelete }) {
   };
 
   const handleDeleteClick = () => {
+    onDelete(id);
     setShowDeleteModal(true);
   };
 
   const handleConfirmDelete = () => {
-    // Perform delete operation here
     onDelete(id);
     setShowDeleteModal(false);
   };  
@@ -152,27 +57,25 @@ function TagsetAccordion({ id, name, description, onUpdate, onDelete }) {
   };
 
   return (
-    <div>
+    <div className='tagset-accordion'>
       <Accordion
-        className='tagset-accordion'
         style={{ backgroundColor: "#E7E5FF", marginBottom: "15px"}}
       >
         <AccordionSummary expandIcon={<ExpandMoreIcon />}>
           <div className='tagset-content'>{name}</div>
           <div className='tagset-content'>{description}</div>
           <div className='tagset-button'>
-            <BookmarkIcon
+            <BookmarkBorderOutlinedIcon
               onClick={handleBookmarkClick}
               style={{ color: isBookmarked ? '#FC5B5C' : 'inherit' }}
             />
-            <EditIcon onClick={handleEditClick} />
-            <DeleteIcon onClick={handleDeleteClick} />
+            <EditOutlinedIcon onClick={handleEditClick} />
+            <DeleteOutlinedIcon onClick={handleDeleteClick} />
           </div>
         </AccordionSummary>
         <AccordionActions>
           <div className='tagset-child-button'>
-            <CreateNewFolderIcon />
-            <NoteAddIcon />
+            <CreateNewFolderOutlinedIcon />
           </div>
         </AccordionActions>
       </Accordion>
--- src/pages/TagsetPage.jsx
+++ src/pages/TagsetPage.jsx
@@ -1,39 +1,54 @@
-import { useState } from 'react';
-import CreateTagsetModal from '../components/CreateTagsetModal';
+import React, { useState } from 'react';
+import { Modal, Button, Form } from 'react-bootstrap';
 import TagsetAccordion from '../components/TagsetAccordion';
-import { Container, Row, Col, Button } from 'react-bootstrap';
+import CreateTagsetModal from '../components/CreateTagsetModal';
+import { Container, Row, Col } from 'react-bootstrap';
 import { BsPlus } from 'react-icons/bs';
+import SideBar from "../components/SideBar";
+import { v4 as uuidv4 } from 'uuid';
 import '../styles/Page.css';
 import '../styles/Tagset.css';
 import '../styles/CreateModal.css';
-import SideBar from "../components/SideBar";
 
 function TagsetPage() {
   const [showCreateModal, setShowCreateModal] = useState(false);
   const [tagsets, setTagsets] = useState([]);
   const [tagName, setTagName] = useState('');
   const [tagDescription, setTagDescription] = useState('');
-  const [nestedTagsetName, setNestedTagsetName] = useState('');
-  const [nestedTagsetDescription, setNestedTagsetDescription] = useState('');
+  const [rootTagsets, setRootTagsets] = useState([]);
+  const [nestedTagsets, setNestedTagsets] = useState({});
 
 
   const handleCreateClick = () => {
     if (tagName.trim() === '' || tagDescription.trim() === '') {
       alert('Please enter name and description.');
     } else {
-      const newTagsetId = Math.random().toString(36).substring(7);
-
+      const newTagsetId = uuidv4();
       const newTagset = {
         id: newTagsetId,
         name: tagName,
         description: tagDescription,
+        label_parent: 'ROOT', // กำหนด label_parent เป็น 'ROOT' สำหรับ Root Tagsets
       };
-
       setTagsets([...tagsets, newTagset]);
+      if (newTagset.label_parent === 'ROOT') {
+        setRootTagsets([...rootTagsets, newTagset]); // เพิ่ม Root Tagset
+      } else {
+        setNestedTagsets(prevState => ({
+          ...prevState,
+          [newTagset.label_parent]: [
+            ...(prevState[newTagset.label_parent] || []),
+            newTagset
+          ]
+        }));
+      }
       setShowCreateModal(false);
+      setTagName('');
+      setTagDescription('');
     }
   };
-
+  
+  
   const handleUpdateTagset = (id, editedName, editedDescription) => {
     const updatedTagsets = tagsets.map(tagset => {
       if (tagset.id === id) {
@@ -47,11 +62,31 @@ function TagsetPage() {
     });
     setTagsets(updatedTagsets);
   };
-
+  
   const handleDeleteTagset = (id) => {
     const updatedTagsets = tagsets.filter(tagset => tagset.id !== id);
     setTagsets(updatedTagsets);
   };
+  
+
+  const handleCreateNestedTagset = (parentTagsetId) => {
+    const newNestedTagsetId = uuidv4();
+    const newNestedTagset = {
+      id: newNestedTagsetId,
+      name: 'Nested Tagset', // กำหนดชื่อ Tagset ของ Nested Tagset
+      description: 'Description of Nested Tagset', // กำหนดคำอธิบายของ Nested Tagset
+      label_parent: parentTagsetId, // กำหนด label_parent เป็น parentTagsetId
+    };
+    setTagsets([...tagsets, newNestedTagset]); // เพิ่ม Nested Tagset เข้าไปใน tagsets
+    setNestedTagsets(prevState => ({
+      ...prevState,
+      [parentTagsetId]: [
+        ...(prevState[parentTagsetId] || []),
+        newNestedTagset
+      ]
+    }));
+  };
+  
 
   return (
     <SideBar>
@@ -79,20 +114,23 @@ function TagsetPage() {
             tagDescription={tagDescription}
             setTagDescription={setTagDescription}
           />
-          {tagsets.map((tagset) => (
+          {/* ส่งฟังก์ชันไปยัง Tagset Accordions */}
+          {rootTagsets.map((tagset) => (
             <TagsetAccordion
               key={tagset.id}
               id={tagset.id}
               name={tagset.name}
               description={tagset.description}
               onUpdate={handleUpdateTagset}
-              onDelete={handleDeleteTagset}
+              onDelete={handleDeleteTagset} // ส่ง handleDeleteTagset ไปยัง TagsetAccordion
+              onCreateNestedTagset={() => handleCreateNestedTagset(tagset.id)}
+              nestedTagsets={nestedTagsets[tagset.id] || []}
             />
           ))}
         </div>
       </Container>
     </SideBar>
-  );
+  );  
 }
 
 export default TagsetPage;
